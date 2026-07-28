47d21bc464f53389ae7d9709b36a5d67fa6e79fc
olivergrant86 <112356511+olivergrant86@users.noreply.github.com>
2026-07-28 10:28:24 -0500

    Create Play data model

---
+++ core/models.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class Play:
+    play_number: int = 0
+
+    quarter: Optional[int] = None
+    clock: str = ""
+
+    down: Optional[int] = None
+    distance: Optional[int] = None
+
+    hash_mark: str = ""
+    field_position: Optional[int] = None
+
+    offense_personnel: str = ""
+    defense_personnel: str = ""
+
+    formation: str = ""
+    strength: str = ""
+
+    motion: str = ""
+    shift: str = ""
+
+    play_type: str = ""
+    run_pass: str = ""
+
+    concept: str = ""
+
+    gain: int = 0
+
+    result: str = ""
+
+    explosive: bool = False
+    success: bool = False
+
+    red_zone: bool = False
+    goal_line: bool = False
+
+    notes: str = ""
